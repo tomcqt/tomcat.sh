b14d02d441ae935a49be683fb0f34c274865ba6a
tomcat with a q <hi@tomcat.sh>
2025-04-03 16:22:44 +0000

    Fix formatting and email link.

--- js/projects.js
+++ js/projects.js
@@ -1,46 +1,46 @@
 let projects = [
-    {
+  {
     name: "adyOS",
     desc: "A full GNU/Linux-inspired pseudo-operating system written in JavaScript. You can view the website at %link%.",
     link: {
-        name: "ady.best",
-        url: "https://ady.best/",
+      name: "ady.best",
+      url: "https://ady.best/",
     },
     source: "https://git.ady.best/",
     time: "Made During: January 2025",
-    },
-    {
+  },
+  {
     name: "Tanuki",
     desc: "A custom social media service that I made over Summer 2024. This project is currently down.",
     link: false,
     source: "https://git.tomcat.sh/tanukitweaks/",
     time: "Made During: Summer 2024",
-    },
-    {
+  },
+  {
     name: "Telatro",
     desc: "A port of Balatro to the web. This project is being hosted at %link%.",
     link: {
-        name: "telatro.tomcat.sh",
-        url: "https://telatro.tomcat.sh/",
+      name: "telatro.tomcat.sh",
+      url: "https://telatro.tomcat.sh/",
     },
     source: "https://git.tomcat.sh/Telatro/",
     time: "Made during: March 2025",
-    },
-    {
+  },
+  {
     name: "tomcat.sh",
     desc: "The website that you are currently on!",
     link: false,
     source: "https://git.tomcat.sh/tomcat.sh",
     time: "Made during: March 2024 through Today",
-    },
-    {
+  },
+  {
     name: "tomcat's braille art workspace",
     desc: "A program that I made to make ASCII art using the Braille characters. This project is currently being hosted at %link%.",
     link: {
-        name: "tomcat.sh/braille",
-        url: "https://www.tomcat.sh/braille",
+      name: "tomcat.sh/braille",
+      url: "https://www.tomcat.sh/braille",
     },
     source: false,
     time: "Made During: December 2023",
-    },
+  },
 ];
