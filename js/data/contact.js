797b2b139020e3016790910fdb8b6bf6f120bca3
tomcat with a q <hi@tomcat.sh>
2025-04-19 11:12:08 -0400

    start work on making the contacts thing work again

--- js/data/contact.js
+++ js/data/contact.js
@@ -1,11 +1,11 @@
-let urls = {
-  email: "mailto:hi@tomcat.sh",
-  github: "https://git.tomcat.sh/",
-  discord: "https://discord.com/users/1059605055411601429",
-  twitter: "https://twitter.com/txmcqt",
-  bluesky: "https://bsky.app/profile/tomcat.sh",
-  reddit: "https://reddit.com/u/tomcatonrblx",
-  youtube: "https://youtube.com/@tomcat.c",
-  twitch: "https://twitch.tv/tomcatsh",
-  steam: "https://steamcommunity.com/id/tomcatsh/",
-};
+let urls = [
+  ["email", "mailto:hi@tomcat.sh", "#ffffff"],
+  ["github", "https://git.tomcat.sh/", "#2da44e"],
+  ["discord", "https://discord.com/users/1059605055411601429", "#7289da"],
+  ["twitter (x)", "https://twitter.com/txmcqt", "#1da1f2"],
+  ["bluesky", "https://bsky.app/profile/tomcat.sh", "#0886fe"],
+  ["reddit", "https://reddit.com/u/tomcatonrblx", "#ff6314"],
+  ["youtube", "https://youtube.com/@tomcat.c", "#ff0000"],
+  ["twitch", "https://twitch.tv/tomcatsh", "#7c56c3"],
+  ["steam", "https://steamcommunity.com/id/tomcatsh/", "#66c0f4"],
+];
